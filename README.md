a7b690f209be1d69865bdab28a392d421e91c089
发明控 <30631809+bilibilifmk@users.noreply.github.com>
2020-06-06 02:46:35 +0800

    v1.0.1

---
+++ README.md
@@ -0,0 +1,97 @@
+# wifi_link_tool
+这是一个为esp8266设计的通用配网库   
+该库免费提供 使用请注明出处 允许毕业设计使用本库 
+## 当前版本v1.0.1 开源协议：GPL3.0
+****
+# 配套视频：https://www.bilibili.com/video/BV1ye411x7Xr/  
+![img](/img/index.jpg)
+****
+## 普通配网流程 1.链接WiFi 2.自动弹出配网或打开浏览器访问6.6.6.6 3.输入WiFi密码   
+## 微信小程序配网 使用微信扫描二维码 打开小程序 点击通用设备配网  输入WiFi密码
+****
+# 关于配网操作
+* 默认情况下 下拉gpio0(nodemcu D3可以按flash按键)5秒 重置已有配网 第一次使用该库请重置一次或用官方工具擦除flash  
+* 默认情况下 状态指示灯gpio02（nodemcu D4） 也就是12f的板载led  
+* 指示灯常亮 未链接网络 处于未配置 或 无法链接网络  
+* 指示灯闪烁 正在抹除配置  
+* 指示灯熄灭 联网正常  
+
+# 微信小程序配网
+## 使用微信扫一扫进行加载小程序
+
+![image](/img/wxxcx.jpg)
+
+## 小程页面
+![image](/img/wx1.jpg)
+![image](/img/wx2.jpg)  
+点击通用设备配网 即可弹出配网 如未弹出请检查文件系统是否上传成功！  
+点击设备列表可搜索局域网中可被搜索的设备
+
+# 普通配网
+## 扫描二维码直接链接或手动链接 SSID：wifi_link_tool
+### 该二维码支持国内大部分手机相机 （例如使用miui相机）
+![image](/img/ptlink.png)  
+链接成功后 会弹出配网web 如为弹出 请检查设备支持性（安卓4.4.4以上大部分支持 ios 11+）和文件系统是否上传成功！
+
+# 安装库wif link tool 库
+## 安装库本身
+* 请保证已经安装了 arduino esp8266 sdk （已在V2.5.0测试）
+* 在github上下载本库
+* 打开arduino  项目—>加载库—>添加.zip库 —>选择刚才下载的zip导入 (手动库安装目录Arduino\libraries\WiFi_link_tool)
+
+## 安装文件系统
+* 下载文件系统上传工具 [工具地址](https://github.com/esp8266/arduino-esp8266fs-plugin/releases)
+* 导入工具参考目录 ******/tools/ESP8266FS/tool/esp8266fs.jar  
+* 重启arduino
+
+## 测试库
+* 打开测试demo 文件—>示例—>wifi_link_tool—>demo
+* 选择串口及波特率
+* 选择合适的flash size
+![image](/img/flash.jpj)  
+* 点击验证 验证库是否能正常编译
+* 检查文件系统上传工具  工具下会显示 ESP8266 sketch Data Upload 证明安装成功
+* 上传文件系统 工具 —>ESP8266 sketch Data Upload 
+
+# 库基础配置及api
+* 配置设备  
+在setup中配置设备io名称  
+    rstb=D3;  
+    //重置io  
+    stateled=D4;  
+    //指示灯io  
+    Hostname = "ESP8266";  
+    //设备名称 允许中文名称 不建议太长  
+    wxscan=true;  
+    //是否被小程序发现设备 开启意味该设备具有后台 true开启 false关闭  
+* load()函数 加载启动等 必须存在
+
+* pant()函数 维持系统正常运行 请保证在loop 函数的第一行 保证系统稳定性
+
+* 局域网中获得设备名称API(用来配合网关或设备通信)    
+该api以集成进库中  
+作用 获得设备设备名称 方便设备联动  
+请求方法 http get 返回类型 String  
+地址 http://x.x.x.x/opera?opera=sb
+
+* 自定义http get请求   
+语法： webServer.on("/<请求地址>", <处理函数>);   
+请写在setup函数中 load函数之后  
+处理函数请自定义一个新函数  
+请参考demo中get请求处理
+
+* 自定义添加web
+默认情况 库自动添加 config.html(配网web) index.html(设备主页)  
+将自定义的html 放入Data目录下（将demo另存为 在另存目录就会出现data目录）  
+语法： webServer.on("/<请求地址>", <加载函数>);  
+请写在setup函数中 load函数之后  
+加载函数 语法 void <加载函数>(){File file = SPIFFS.open("/<自定义html文件名>", "r"); webServer.streamFile(file, "text/html");file.close(); }  
+请参考demo中加载多个web
+
+
+
+
+
+
+## 测试环境  sdk版本：2.5.0 arduino版本1.8.8
+
---
+++ wifi_link_tool.h
@@ -0,0 +1,313 @@
+/*
+wifi link tool 配网库
+by:发明控 
+版本v1.0.1
+测试环境 sdk版本：2.5.0 arduino版本1.8.8
+项目地址：https://github.com/bilibilifmk/wifi_link_tool 项目成员：发明控 狗腿 
+*/
+
+
+
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+#include <FS.h>
+#include <DNSServer.h>
+#include <ESP8266mDNS.h>
+const char *AP_name = "wifi_link_tool"; //修改后即不支持微信配网
+
+
+
+/////////////////////////////////////////////////////不建议修改部分//////////////////////////////////////////////////////////////////
+String Hostname = "ESP8266";     
+String url = Hostname + ".com";   
+int Signal_filtering = -200;  
+const byte DNS_PORT = 53;
+String WiFi_State;   
+#define WiFi_State_Addr 0   
+bool wxscan=true;
+int rstb=D3;
+int stateled=D4;
+IPAddress apIP(6, 6, 6, 6);
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+String responseHTML =" <html><head><title>正在跳转</title></head><body><script language='javascript'>document.location = '/'</script></body></html>";
+
+
+//心跳服务
+void pant(){
+   MDNS.update();
+   dnsServer.processNextRequest();
+   webServer.handleClient();
+           }
+
+
+
+
+void wwwroot()
+{
+  
+  if (WiFi_State == "1")
+  {
+    File file = SPIFFS.open("/index.html", "r");
+    webServer.streamFile(file, "text/html");
+    file.close();
+  }
+  else if (WiFi_State == "0")
+  {
+    File file = SPIFFS.open("/config.html", "r");
+    webServer.streamFile(file, "text/html");
+    file.close();
+  }
+}
+
+
+void wifiConfig()
+{
+ 
+  if (webServer.hasArg("ssid") && webServer.hasArg("password") && WiFi_State == "0")
+  {
+    int ssid_len = webServer.arg("ssid").length();
+    int password_len = webServer.arg("password").length();
+  
+    if ((ssid_len > 0) && (ssid_len < 33) && (password_len > 7) && (password_len < 65))
+    {
+      
+      String ssid_str = webServer.arg("ssid");
+      String password_str = webServer.arg("password");
+      const char *ssid = ssid_str.c_str();
+      const char *password = password_str.c_str();
+      Serial.print("SSID: ");
+      Serial.println(ssid);
+      Serial.print("Password: ");
+      Serial.println(password);
+    
+      WiFi.begin(ssid, password);
+      Serial.print("Connenting");
+    
+      unsigned long millis_time = millis();
+      while ((WiFi.status() != WL_CONNECTED) && (millis() - millis_time < 8000))
+      {
+        delay(500);
+        Serial.print(".");
+      }
+ 
+      if (WiFi.status() == WL_CONNECTED)
+      {
+       
+        digitalWrite(stateled, HIGH);
+        Serial.println("");
+        Serial.println("Connected successfully!");
+        Serial.print("IP Address: ");
+        Serial.println(WiFi.localIP());
+        Serial.print("http://");
+        Serial.println(Hostname);
+        webServer.send(200, "text/plain", "1");
+        delay(300);
+        WiFi_State = "1";
+      
+        EEPROM.write(WiFi_State_Addr, 1);
+        EEPROM.commit();
+        delay(50);
+    
+        WiFi.softAPdisconnect();
+  
+      }
+      else
+      {
+        
+        Serial.println("Connenting failed!");
+        webServer.send(200, "text/plain", "0");
+      }
+    }
+    else
+    {
+     
+      Serial.println("Password format error");
+      webServer.send(200, "text/plain", "Password format error");
+    }
+  }
+  else
+  {
+   
+    Serial.println("Request parameter error");
+    webServer.send(200, "text/plain", "Request parameter error");
+  }
+}
+
+
+String wifi_type(int typecode)
+{
+  if (typecode == ENC_TYPE_NONE) return "Open";
+  if (typecode == ENC_TYPE_WEP) return "WEP ";
+  if (typecode == ENC_TYPE_TKIP) return "WPA ";
+  if (typecode == ENC_TYPE_CCMP) return "WPA2";
+  if (typecode == ENC_TYPE_AUTO) return "WPA*";
+}
+
+
+void wifiScan()
+{
+  String req_json = "";
+  Serial.println("Scan WiFi");
+  int n = WiFi.scanNetworks();
+  if (n > 0)
+  {
+    int m = 0;
+    req_json = "{\"req\":[";
+    for (int i = 0; i < n; i++)
+    {
+    if ((int)WiFi.RSSI(i) >= Signal_filtering)
+     //  if (1)
+      {
+        m++;
+        String a="{\"ssid\":\"" + (String)WiFi.SSID(i) + "\"," + "\"encryptionType\":\"" + wifi_type(WiFi.encryptionType(i)) + "\"," + "\"rssi\":" + (int)WiFi.RSSI(i) + "},";
+        if(a.length()>15)
+        req_json += a;  
+        }
+    }
+    req_json.remove(req_json.length() - 1);
+  
+    req_json += "]}";
+    webServer.send(200, "text/json;charset=UTF-8", req_json);
+    Serial.print("Found ");
+    Serial.print(m);
+    Serial.print(" WiFi!  >");
+    Serial.print(Signal_filtering);
+    Serial.println("dB");
+  }
+}
+
+void opera() {
+  if(webServer.arg("opera") == "sb"){
+    
+  webServer.send(200, "text/plain", Hostname);
+    
+  }
+  
+}
+
+
+
+void blink() {
+  Serial.println("Keep 3 seconds to reset!");
+  bool res_state = true;
+  unsigned int res_time = millis();
+  
+  while (millis() - res_time < 3000)
+  {
+    if (digitalRead(rstb) != LOW)
+    {
+      res_state = false;
+      Serial.println("Cancel reset!");
+      break;
+    }
+  }
+  if (res_state == true)
+  {
+    WiFi.disconnect(true);
+    delay(100);
+    EEPROM.write(WiFi_State_Addr, 0);
+    EEPROM.commit();
+    delay(300);
+    Serial.println("Press the 'rst' button!");
+    while (1) {
+      digitalWrite(stateled, LOW);
+      delay(250);
+      digitalWrite(stateled, HIGH);
+      delay(250);
+    }
+     Serial.println("重置!");
+     ESP.restart();
+  }
+}
+
+
+//加载部分
+void load(){ 
+  attachInterrupt(digitalPinToInterrupt(rstb), blink, FALLING);
+  Serial.println("");
+  EEPROM.begin(4096);
+  SPIFFS.begin();
+  WiFi.hostname(Hostname);
+  pinMode(stateled, OUTPUT);
+  WiFi_State = EEPROM.read(WiFi_State_Addr);
+  delay(300);
+  if (WiFi_State == "1")  
+  {
+    Serial.println("找到配置!");
+    Serial.print("链接网络");
+    unsigned millis_time = millis();
+    while ((WiFi.status() != WL_CONNECTED) && (millis() - millis_time < 5000))
+    {
+      delay(250);
+      Serial.print(".");
+    }
+    if (wxscan){
+      if (MDNS.begin(Hostname)) { Serial.println("mDNS以启动"); }
+      MDNS.addService("http", "tcp", 80);
+                }
+
+    Serial.println("");
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.print("IP 地址: ");
+      Serial.println(WiFi.localIP());
+
+      Serial.print("http://");
+      Serial.println(Hostname);
+      digitalWrite(stateled, HIGH);
+    }
+    else
+    {
+      Serial.println("链接失败!");
+      Serial.println("请尝试重置系统!");
+      digitalWrite(stateled, LOW);
+    }
+  }
+  else if (WiFi_State == "0")   
+  {
+   
+    digitalWrite(stateled, LOW);
+    WiFi.disconnect(true);
+    Serial.println("");
+    Serial.print("启动WiFi配置 \n建立AP 名称 -->  ");
+    Serial.println(AP_name);
+    Serial.print("http://");
+    Serial.println(url);
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(AP_name);
+  }
+  else
+  {
+    Serial.println("初次启动");
+    EEPROM.write(WiFi_State_Addr, 0);
+    EEPROM.commit();
+    delay(300);
+   
+    Serial.println("请重置系统！");
+    while (1) {
+      digitalWrite(stateled, LOW);
+      delay(250);
+      digitalWrite(stateled, HIGH);
+      delay(250);
+    }
+  }
+
+
+
+  Serial.println("启动http服务");
+  webServer.on("/", wwwroot);
+  webServer.on("/wificonfig", wifiConfig);
+  webServer.on("/wifiscan", wifiScan);
+  webServer.on("/opera", opera);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  dnsServer.start(DNS_PORT, "*", apIP);
+  webServer.onNotFound([]() {
+      webServer.send(200, "text/html", responseHTML);
+    });
+    webServer.begin();
+  }
